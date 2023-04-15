80d788aa320f6852581efaf8f5b1ae506a5c40da
michalmolka <70074287+michalmolka@users.noreply.github.com>
2023-04-15 09:22:49 +0200

    Add files via upload

---
+++ csharp_fsharp_assembly/Program_csharp.cs
@@ -0,0 +1,13 @@
+﻿using FsharpNamespace;
+
+namespace csharp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        FsharpClass fsharpclass = new FsharpClass("Some info", 5);
+        fsharpclass.FsharpMethod01(4);
+        fsharpclass.FsharpMethod02(2,5);
+        Console.WriteLine($"The value returned by the second method is: {fsharpclass.FsharpMethod02(1,7)}");
+    }
+}
---
+++ csharp_fsharp_assembly/Program_fsharp.fs
@@ -0,0 +1,24 @@
+﻿namespace FsharpNamespace
+
+type FsharpClass(value01:string, value02:int) =
+    member this.Value01 = value01
+    member this.Value02 = value02
+
+    member this.FsharpMethod01(value03) =
+        let value04 = this.Value02 + value03
+        let matchValues x =
+            match x with
+            | i when i < 0 -> printfn "Low"
+            | i when i >= 0 && i < 10 -> printfn "Medium"
+            | i when i >= 10 && i < 100-> printfn "High"
+            | _ -> printfn "Outside the range. Additional info: %s." this.Value01
+        matchValues value04
+
+    member this.FsharpMethod02(firstValue, secondValue) =
+        let listToFilter = [firstValue..secondValue]
+        let listFiltered = listToFilter |> List.filter(fun i -> i > 4) |> List.sumBy(fun i -> i)
+        listFiltered
+
+// let firstObject = FsharpClass("Some info", 2)
+// firstObject.FsharpMethod01(4000)
+// printfn "The valuer returned by the second method is: %d" (firstObject.FsharpMethod02(1,7))
