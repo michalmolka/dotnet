44e31f9f79cc523a7dd6af7116d5d5b2a1f86869
michalmolka <70074287+michalmolka@users.noreply.github.com>
2023-04-15 15:08:00 +0200

    Add files via upload

---
+++ cosmosdb_change_feed/IowaSales.cs
@@ -0,0 +1,31 @@
+namespace CosmosDB1
+{
+    public class IowaSales
+    {
+        public string? Invoice { get; set; }
+        public int Date { get; set; }
+        public int StoreNumber { get; set; }
+        public string? StoreName { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public int ZipCode { get; set; }
+        public int CountyNumber { get; set; }
+        public string? CountyName { get; set; }
+        public int CategoryNumber { get; set; }
+        public string? CategoryName { get; set; }
+        public int VendorNumber { get; set; }
+        public string? VendorName { get; set; }
+        public int ItemNumber { get; set; }
+        public string? ItemName { get; set; }
+        public int Pack { get; set; }
+        public int BottleVolume { get; set; }
+        public double StateBottleCost { get; set; }
+        public double StateBootleRetail { get; set; }
+        public int BootlesSold { get; set; }
+        public double SaleDollars { get; set; }
+        public double VolumeSoldLiters { get; set; }
+        public double VolumeSoldGallons { get; set; }
+        public string? id { get; set; }
+
+    };
+}
---
+++ cosmosdb_change_feed/Program_change_feed.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace CosmosDB1;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string endpointUrl = "your-cosmos-db-endpoint";
+        string authorizationKey = "your-authorization-key";
+
+        CosmosClient cosmosClient = new CosmosClient(endpointUrl, authorizationKey);
+        Database database = cosmosClient.GetDatabase("cosmos_db_iowa");
+        Container sourceContainer = database.GetContainer("cosmos_c_iowa");
+        Container leaseContainer = database.GetContainer("cosmos_c_lease");
+        Container backUpContainer = database.GetContainer("cosmos_c_iowa_backup00");
+
+        ChangeFeedProcessor processor = await StartChangeFeedProcessorAsync();
+        Console.ReadKey();
+        await processor.StopAsync();
+
+        async Task<ChangeFeedProcessor> StartChangeFeedProcessorAsync()
+        {
+            ChangeFeedProcessor changeFeedProcessor = sourceContainer
+                .GetChangeFeedProcessorBuilder<IowaSales>("cosmosBackup", HandleChangesAsync)
+                .WithInstanceName("cosmosBackup-01")
+                .WithLeaseContainer(leaseContainer)
+                .Build();
+
+            Console.WriteLine("Starting Change Feed Processor...");
+            await changeFeedProcessor.StartAsync();
+            return changeFeedProcessor;
+        }
+
+        async Task HandleChangesAsync(
+            ChangeFeedProcessorContext context,
+            IReadOnlyCollection<IowaSales> changes,
+            CancellationToken cancellationToken)
+        {
+            foreach (IowaSales item in changes)
+            {
+                await backUpContainer.CreateItemAsync(
+                    item,
+                    partitionKey: new PartitionKey(item.CountyName)
+                );
+                Console.WriteLine($"Item added to backup container: {item.id}");
+                await Task.Delay(10);
+            }
+        }
+    }
+}
