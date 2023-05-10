f112d98392e3f8191212fa374ac88c9e672753af
michalmolka <70074287+michalmolka@users.noreply.github.com>
2023-05-10 20:09:35 +0200

    Add files via upload

---
+++ key_vault.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
+
+namespace key_vault;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var credential = new ClientSecretCredential(AzureVariables.tenantId, AzureVariables.clientId, AzureVariables.clientSecret);
+        var client = new SecretClient(new Uri(AzureVariables.keyVaultUri), credential);
+        var secret = await client.GetSecretAsync("cosmosdb-primary");
+        Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
+    }
+}
