cb28c89db0d8301b8930e44ea5cc12fde0be9729
michalmolka <70074287+michalmolka@users.noreply.github.com>
2023-05-14 12:28:57 +0200

    Add files via upload

---
+++ adls.cs
@@ -0,0 +1,75 @@
+﻿using Azure;
+using Azure.Storage.Files.DataLake;
+using Azure.Storage.Files.DataLake.Models;
+using Azure.Identity;
+
+// dotnet add package Azure.Storage.Files.DataLake
+// dotnet add package Azure.Identity
+
+namespace adls;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Create a dataLakeServiceClient
+        var credential = new ClientSecretCredential(AzureVariables.tenantId, AzureVariables.clientId, AzureVariables.clientSecret);
+        Uri dataLakeEndpoint = new Uri(AzureVariables.adlsEndpoint);
+        DataLakeServiceClient dataLakeServiceClient = new DataLakeServiceClient(dataLakeEndpoint, credential);
+
+        // Create a dataLakeFileSystemClient
+        string containerName = "iowa-json";
+        DataLakeFileSystemClient dataLakeFileSystemClient = dataLakeServiceClient.GetFileSystemClient(containerName);
+
+        // Create a new folder
+        DataLakeDirectoryClient dataLakeDirectoryClient00 = await dataLakeFileSystemClient.CreateDirectoryAsync("adls_json_upload");
+
+        System.Console.WriteLine($"Directory: {dataLakeDirectoryClient00.Name} created");
+
+        // Rename a folder
+        DataLakeDirectoryClient dataLakeDirectoryClient01 = dataLakeFileSystemClient.GetDirectoryClient("adls_json_upload");
+        DataLakeDirectoryClient dataLakeDirectoryClient02 = await dataLakeDirectoryClient01.RenameAsync("adls-json_upload-01");
+        System.Console.WriteLine($"Directory {dataLakeDirectoryClient01.Name} has been renamed. New name: {dataLakeDirectoryClient02.Name}");
+
+        // // Delete the directory
+        // await dataLakeDirectoryClient02.DeleteAsync();
+
+        // Upload a file
+        string fileName = "09.json";
+        string fileToUpload = Path.Combine("json_files", fileName);
+
+        DataLakeFileClient fileToUploadClient = await dataLakeDirectoryClient02.CreateFileAsync(fileName);
+
+        FileStream fileStream00 = File.OpenRead(fileToUpload);
+        await fileToUploadClient.AppendAsync(fileStream00, offset: 0);
+        await fileToUploadClient.FlushAsync(position: fileStream00.Length);
+
+        // List files in a directory
+        await foreach (PathItem pathItem in dataLakeDirectoryClient02.GetPathsAsync(recursive: true))
+        {
+            System.Console.WriteLine($"File: {pathItem.Name}");
+        }
+
+        // Download/read a file
+        DataLakeFileClient fileToDownload = dataLakeDirectoryClient02.GetFileClient("09.json");
+        Response<FileDownloadInfo> downloadResponse  = await fileToDownload.ReadAsync();
+        BinaryReader reader = new BinaryReader(downloadResponse.Value.Content);
+        FileStream fileStream01 = File.OpenWrite("json_files/09-01.json");
+
+        byte[] buffer = new byte[4096];
+        int count;
+
+        while((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+        {
+            fileStream01.Write(buffer, 0, count);
+        }
+
+        await fileStream01.FlushAsync();
+        fileStream01.Close();
+
+        // Rename a file and move to another folder - the new folder must exist
+        await fileToDownload.RenameAsync("adls-json_upload-02/09-03.json");
+
+        // Delete a file
+        // await dataLakeFileSystemClient.DeleteFileAsync("adls-json_upload-01/09.json");
+    }
+}
---
+++ blob.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Identity;
+
+// dotnet add package Azure.Storage.Blobs 
+// dotnet add package Azure.Identity
+
+namespace blob;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Create a BlobServiceClient
+        var credential = new ClientSecretCredential(AzureVariables.tenantId, AzureVariables.clientId, AzureVariables.clientSecret);
+        Uri blobEndpoint = new Uri(AzureVariables.blobEndpoint);
+        BlobServiceClient blobServiceClient = new BlobServiceClient(blobEndpoint, credential);
+
+        // Create a BlobContainerClient
+        string containerName = "iowa-json";
+        BlobContainerClient container = blobServiceClient.GetBlobContainerClient(containerName);
+
+        // Upload a file
+        // Specify a file to upload
+        string fileName00 = "05.json";
+        string fileToUpload = Path.Combine("json_files", fileName00);
+        
+        // Create a BlobClient for the file
+        BlobClient blobClient00 = container.GetBlobClient(fileName00);
+
+        // Upload the file
+        var uploadBlob = await blobClient00.UploadAsync(fileToUpload, true);
+        System.Console.WriteLine($"Status: {uploadBlob.GetRawResponse().Status}, ReasonPhrase: {uploadBlob.GetRawResponse().ReasonPhrase}");
+        
+        // List files in a container
+        Azure.AsyncPageable<BlobItem> blobContainerList = container.GetBlobsAsync();
+
+        await foreach (BlobItem  item in blobContainerList)
+        {
+            System.Console.WriteLine($"BLOB name: {item.Name}, Size: {item.Properties.ContentLength} bytes");
+        }
+
+        // Download a file from a container
+        // Create a BlobClient for a downloaded file
+        string fileName01 = "08.json";
+        BlobClient blobClient01 = container.GetBlobClient(fileName01);
+        // Specify a location for the downloaded file
+        string downloadFile = Path.Combine("json_Files", fileName01);
+        var downloadBlob = await blobClient01.DownloadToAsync(downloadFile);
+
+        // Remove the file
+        // var removeBlob = await blobClient01.DeleteAsync();
+    }
+}
